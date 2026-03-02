bf88c4ddcd478e7eb5dac3d8f6602b780f79d0c7
Danil1325 <danil.strelciuc@gmail.com>
2026-03-02 18:12:04 +0200

    Resolve conflict

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { AiOutlineUser } from "react-icons/ai"
+
 import './header.scss'
 
-<<<<<<< HEAD:src/Components/Header.tsx
 type NavItem = { name: string; href: string; current: boolean }
 
 const navigation: NavItem[] = [
     { name: 'Dashboard', href: '#', current: true },
-=======
-const navigation = [
-    { name: 'Home', href: '#', current: true },
-    { name: 'About', href: '#', current: false },
-    { name: 'Shop', href: '#', current: false },
-    { name: 'Pages', href: '#', current: false },
->>>>>>> 7678e441cdeee6c9512d174bce31b7c6ac4ed756:src/Components/Header.jsx
     { name: 'Team', href: '#', current: false },
     { name: 'Blog', href: '#', current: false },
 ]
@@ -26,123 +16,7 @@ function classNames(...classes: Array<string | false | null | undefined>) {
 
 function Header(): React.ReactElement {
     return (
-        <>
-            <Disclosure
-                as="nav"
-                className="relative base-color after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 border-1 rounded-3xl m-4"
-            >
-                <div className="w-full px-2 sm:px-6 lg:px-8">
-                    <div className="relative flex h-16 items-center justify-between">
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            {/* Mobile menu button*/}
-                            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-                                <span className="absolute -inset-0.5" />
-                                <span className="sr-only">Open main menu</span>
-                                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-                            </DisclosureButton>
-                        </div>
-                        <div className="flex flex-1 items-center justify-start">
-                            <div className="flex shrink-0 items-center">
-                                <img
-                                    alt="Your Company"
-                                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                                    className="h-8 w-auto"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="hidden sm:block absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <div className="flex space-x-4">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        aria-current={item.current ? 'page' : undefined}
-                                        className={classNames(
-                                            item.current ? 'text-black hover:text-indigo-500' : 'text-black hover:bg-white/5 hover:text-indigo-500',
-                                            'rounded-md px-3 py-2 text-sm font-medium',
-                                        )}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="ml-auto flex items-center pr-2 sm:pr-0">
-                            <button
-                                type="button"
-                                className="relative rounded-full p-1 text-black hover:text-indigo-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
-                            >
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">View notifications</span>
-                                <BellIcon aria-hidden="true" className="size-6" />
-                            </button>
-
-                            {/* Profile dropdown */}
-                            <Menu as="div" className="relative ml-3">
-                                <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                                    <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">Open user menu</span>
-                                    <a className={'text-indigo-500 bg-white rounded-2xl p-1 flex items-center gap-2'}>Log in <AiOutlineUser />
-                                    </a>
-                                </MenuButton>
-
-                                <MenuItems
-                                    transition
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md base-color py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                                >
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm base-color text-black data-focus:bg-white/5 data-focus:outline-hidden hover:text-indigo-500"
-                                        >
-                                            Your profile
-                                        </a>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm base-color text-black data-focus:bg-white/5 data-focus:outline-hidden hover:text-indigo-500"
-                                        >
-                                            Settings
-                                        </a>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm base-color text-black data-focus:bg-white/5 data-focus:outline-hidden hover:text-indigo-500"
-                                        >
-                                            Sign out
-                                        </a>
-                                    </MenuItem>
-                                </MenuItems>
-                            </Menu>
-                        </div>
-                    </div>
-                </div>
-
-                <DisclosurePanel className="sm:hidden">
-                    <div className="space-y-1 px-2 pt-2 pb-3">
-                        {navigation.map((item) => (
-                            <DisclosureButton
-                                key={item.name}
-                                as="a"
-                                href={item.href}
-                                aria-current={item.current ? 'page' : undefined}
-                                className={classNames(
-                                    item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                                    'block rounded-md px-3 py-2 text-base font-medium',
-                                )}
-                            >
-                                {item.name}
-                            </DisclosureButton>
-                        ))}
-                    </div>
-                </DisclosurePanel>
-            </Disclosure>
-        </>
-
+        <h1 className="text-2xl font-bold">Warehouse Management System</h1>
     );
 }
 
