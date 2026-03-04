e305d3017a0feb5717baf70a40e71169c6d879fe
Danil1325 <danil.strelciuc@gmail.com>
2026-03-04 20:34:53 +0200

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from 'sonner';
 import { CartProvider } from './app/context/CartContext';
 import { Layout } from './app/Components/Layout';
 import { Home } from './app/pages/Home';
+import { Products } from './app/pages/Products';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Layout>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
         </Routes>
       </Layout>
       <Toaster position="top-right" richColors />
